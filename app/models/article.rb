299979663da0ef4c519940807c550f2822233c84
SimonS2019 <shentiexiong@gmail.com>
2023-11-03 15:10:21 +1300

    82 validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,18 @@
 class Article < ApplicationRecord
+ validates :title, presence: true
+ #reload!
+ #this above line is used to reload the model
+
+ #article = Article.new
+ #article.save
+ #article.valid?
+#article.errors
+#article.errors.full_messages=>["Title can't be blank"]
+validates :description, presence: true, length: {minimum: 10, maximum: 300}
+#article.title = "My third article"
+#article.description = "This is my third article"
+
  
+# article = Article.new(title: "a", description: "b")
+# article = Article.new(title: "a", description: "this is my forth article")
 end
