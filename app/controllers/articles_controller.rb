ad0302b16af6e79dbad4bb1c883a3ccaf35c3ff4
SimonS2019 <shentiexiong@gmail.com>
2023-11-06 09:36:43 +1300

    92

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
     end
 
     def new
-        # @article = Article.new
+        @article = Article.new
     end
 
     def create
@@ -22,8 +22,15 @@ class ArticlesController < ApplicationController
         # the above line will create a new article
         # @article = Article.new(article_params)
         # render plain: params[:article]
-        @article.save
+        if @article.save
+            flash[:notice] = "Article was created successfully"
+            # the above line will show the flash message
+            # a flash message is a message that will be shown only once
         redirect_to @article
+        else
+            render 'new'
+            #the above line will render the new template
+        end
         # redirect_to article_path(@article)
     end
 end
