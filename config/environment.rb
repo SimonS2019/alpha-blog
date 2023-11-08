09b2aedce5a6c5460477ecb3d80d15077a1c5ae9
SimonS2019 <shentiexiong@gmail.com>
2023-11-08 15:39:07 +1300

    118

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#The following code is to remove the field_with_errors div that Rails automatically generates when a form field has an error.
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    html_tag.html_safe
+  end
