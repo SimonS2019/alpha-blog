83db1d2727328bb4163d1bf857b437a6ef22b0b0
SimonS2019 <shentiexiong@gmail.com>
2023-11-08 17:03:50 +1300

    126

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+    
+    # User.create(username: "john", email: "john@examplecom")
+    # user = User.first
+    # user.update(username: "john", email: "john2@examplecom")
+    # user.save
+end
