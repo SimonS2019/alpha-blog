1910ac9ea05dfb5f35bff85b3615e176f456f9b8
SimonS2019 <shentiexiong@gmail.com>
2023-11-09 14:06:33 +1300

    128

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ApplicationRecord
-    
-    # User.create(username: "john", email: "john@examplecom")
-    # user = User.first
-    # user.update(username: "john", email: "john2@examplecom")
+    validates :username, presence: true, 
+    uniqueness: { case_sensitive: false }, 
+    length: { minimum: 3, maximum: 25 }
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+validates :email, presence: true, 
+    uniqueness: { case_sensitive: false }, 
+    length: { maximum: 105 },
+    format: { with: VALID_EMAIL_REGEX }
+    # user = User.new(username: "si", email: "simon@example.com")
+    # user = User.new(username: "simon", email: "simon@example.com")
+    # user.valid?
+    # user.errors.full_messages
     # user.save
 end
